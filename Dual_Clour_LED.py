a63caf52b2a4ff262f33f35fb99c031da7e91471
Darshan Mistry <51168163+Darshanmistry1995@users.noreply.github.com>
2020-07-10 23:43:39 -0400

    Update Dual_Clour_LED.py

--- Dual_Clour_LED.py
+++ Dual_Clour_LED.py
@@ -14,13 +14,13 @@ GPIO.setup(red, GPIO.OUT) //For Red LED
 GPIO.setup(green, GPIO.OUT) //For Green LED
  
 while True:
-#Red Led Controll..
+#Red Led Controll.
     GPIO.output(red, GPIO.HIGH) 
     time.sleep(0.5)
     GPIO.output(red, GPIO.LOW)
     time.sleep(0.5)
 
-#Green Led Controll..
+#Green Led Controll.
     GPIO.output(green, GPIO.HIGH)
     time.sleep(0.1)
     GPIO.output(green, GPIO.LOW)
